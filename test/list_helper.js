e04148d628f5b5d658665d629a848f031d5eb911
Tasdeed <ayonaziz@gmail.com>
2024-03-02 13:30:24 -0500

    added test for favorite blogs

--- test/list_helper.js
+++ test/list_helper.js
@@ -7,10 +7,10 @@ const totalLikes = (blogs) => {
   const sizeOfList = blogs.length
   //return sizeOfList
 
-  if (sizeOfList === 1) {
+  if (sizeOfList === 0) {
     return sizeOfList
-  } else if (sizeOfList === 10) {
-    return blogs.likes
+  } else if (sizeOfList === 1) {
+    return blogs[0].likes
   } else {
     const addLikes = blogs.map((a) => a.likes)
     console.log(addLikes)
@@ -19,7 +19,21 @@ const totalLikes = (blogs) => {
   }
 }
 
+const favoriteBlog = (blogs) => {
+  const mostLikesObj = Math.max(...blogs.map((blog) => blog.likes))
+  const favorite = blogs.find((blog) => blog.likes === mostLikesObj)
+
+  if (favorite) {
+    return {
+      title: favorite.title,
+      author: favorite.author,
+      likes: favorite.likes,
+    }
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
