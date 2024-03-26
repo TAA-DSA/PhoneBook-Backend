047149a818bb553e191d7c8a0c55befc51cffc42
Tasdeed <ayonaziz@gmail.com>
2024-03-26 01:37:18 -0400

    implemented json web token for user login

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const indexController = require('../controllers/index.controller')
+const loginRouter = require('../controllers/login')
 const router = express.Router()
 
 router.get('/info', indexController.indexPage)
@@ -10,5 +11,6 @@ router.post('/api/persons', indexController.createContact)
 router.delete('/api/persons/:id', indexController.deleteAllContact)
 router.post('/api/users', indexController.userPath)
 router.get('/api/users', indexController.getUsers)
+router.post('/api/login', loginRouter)
 
 module.exports = router
