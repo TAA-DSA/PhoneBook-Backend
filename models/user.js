c0db703685e93ce864cf008494020332d221843a
Tasdeed <ayonaziz@gmail.com>
2024-03-26 23:21:52 -0400

    update login route

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
     unique: true,
   },
   name: String,
-  password: String,
+  passwordHash: String,
   contact: [
     {
       type: mongoose.Schema.Types.ObjectId,
